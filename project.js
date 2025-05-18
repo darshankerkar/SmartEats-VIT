7cf15c4da1887bf9b8ed1cf0047d988d60d43182
darshankerkar <darshankerkar09@gmail.com>
2025-05-19 00:52:55 +0530

    Add files via upload

--- project.js
+++ project.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let search_btn=document.querySelector("#searchbtn");
 
 search_btn.addEventListener("click",()=>{
@@ -28,3 +29,35 @@ terms_conditions.addEventListener("click",()=>{
 //       alert("You pressed Enter!");
 //     }
 //   });
+=======
+let search_btn=document.querySelector("#searchbtn");
+
+search_btn.addEventListener("click",()=>{
+    alert("Searching menu!!")
+})
+
+let privacy_policy=document.querySelector("#privacy")
+let pp=document.querySelector("#pp")
+
+privacy_policy.addEventListener("click",()=>{
+    pp.style.visibility="visible";
+    Terms.style.visibility="hidden";
+})
+
+let terms_conditions=document.querySelector("#T_C")
+let Terms=document.querySelector("#Terms")
+
+terms_conditions.addEventListener("click",()=>{
+    Terms.style.visibility="visible";
+    pp.style.visibility="hidden";
+})
+
+
+// let searchbox=document.querySelector("#text")
+
+// searchBox.addEventListener("keydown", function(event) {
+//     if (event.key === "Enter") {
+//       alert("You pressed Enter!");
+//     }
+//   });
+>>>>>>> f117536e3a54f8cbda27283dab5711a63b454551
